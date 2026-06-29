793efe30d1aa8eb5298bff4b1b7286be16a7cd65
Pedro M. E. <pedro.miranda.esteves1@gmail.com>
2026-06-29 22:50:48 +0200

    Mejora flame speed vs phi: - phi_list concentra más puntos en el centro del intervalo - Usar la solución del bucle anterior como initial guess para resolver llama . Tiempo reducido a la mitad.

--- S_vs_phi.py
+++ S_vs_phi.py
@@ -4,6 +4,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scienceplots
 plt.style.use(['science'])
+import time
+start_time = time.time()
 
 
 gas_mix_incomplete = ct.Solution('gri30.yaml')
@@ -13,7 +15,13 @@ T_0 = 298 # K
 p = ct.one_atm
 
 vel_list = []
-phi_list = np.linspace(0.5, 1.8, 14)
+
+phi_begin = 0.5
+phi_end = 1.8
+N = 20
+phi_list = [phi_begin + (phi_end-phi_begin) * (n/(N-1) + np.sin(2*np.pi*n/(N-1))/(2*np.pi)) for n in range(N)]
+# Puntos que se concentran en el centro del intervalo para tener precisión en zona curva
+
 for j, phi in enumerate(phi_list):
     # Contador j no se usa
     print(f"\033[1;36m### EQUIVALENCE RATIO: {phi} ###\033[0m")
@@ -33,13 +41,23 @@ for j, phi in enumerate(phi_list):
         # supera el 6% de la máx diferencia del perfil, añade puntos intermedios.
     # print(flame.grid)
 
+    if phi != phi_begin:
+        flame.set_initial_guess(data=flame_sol_previa)
+        # Uso solución de llama con el phi anterior como punto de partida para este
+        # Opcional. Tiempo cómputo se reduce 49%.
+
     flame.solve(loglevel=1, refine_grid=True, auto=True)
         # Método del objeto flame que resuelve ecs. de fluidos en dif. finitas.
+    flame_sol_previa = flame.to_array()
+        # Guardo la solución de un bucle en esta variable.
 
     print(f"\033[1;36m### LAMINAR BURNING SPEED: {flame.velocity[0]*100} cm/s ###\033[0m")
 
     vel_list.append(flame.velocity[0] * 100) # cm/s
         # flame.velocity[0] = velocidad en primer grid point (inlet).
+    print("Run time %s seconds ---" % (time.time() - start_time))
+
+print("Complete run time --- %s seconds ---" % (time.time() - start_time))
 
 
 #%% Plot Speed - phi
@@ -47,8 +65,8 @@ plt.figure(figsize=(8,8))
 
 plt.plot(phi_list,
         vel_list,
-        label="Methane laminar premixed flame"
-        )
+        label="Methane laminar premixed flame",
+        marker="o")
 
 plt.grid(True, which='both', alpha=0.5)
 
